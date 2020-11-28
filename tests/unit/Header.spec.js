7dbcdfd7b0b8d822403fdaacebdee7eaca2d7ada
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-11-28 03:40:42 +0100

    Firest Header component test

---
+++ tests/unit/Header.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from "@vue/test-utils";
+import Header from "@/components/Header.vue";
+
+describe("Header.vue", () => {
+  const title = "Country App";
+  const wrapper = shallowMount(Header).find('[class="header__brand"]');
+  it("renders app title properly", () => {
+    expect.assertions(1);
+    expect(wrapper.text()).toMatch(title);
+  });
+});
