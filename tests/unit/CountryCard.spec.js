648f39646eae33c6f738de3e64a282d771de93d6
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-11-28 03:28:23 +0100

    Unit testing experiments

--- tests/unit/CountryCard.spec.js
+++ tests/unit/CountryCard.spec.js
@@ -2,11 +2,12 @@ import { shallowMount } from "@vue/test-utils";
 import CountryCard from "@/components/CountryCard.vue";
 
 describe("CountryCard.vue", () => {
+  const country = "Italy";
+  const wrapper = shallowMount(CountryCard).find(
+    '[class="country-card__headline"]'
+  );
   it("renders country name properly", () => {
-    const country = "Italy";
-    const wrapper = shallowMount(CountryCard).find(
-      '[class="country-card__headline"'
-    );
+    expect.assertions(1);
     expect(wrapper.text()).toMatch(country);
   });
 });
