b61f0dc23c9cfae407d104f4d07f4ea0dbae7c5c
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-11-28 15:40:07 +0100

    CountryCard component testing

--- tests/unit/CountryCard.spec.js
+++ tests/unit/CountryCard.spec.js
@@ -2,12 +2,21 @@ import { shallowMount } from "@vue/test-utils";
 import CountryCard from "@/components/CountryCard.vue";
 
 describe("CountryCard.vue", () => {
-  const country = "Italy";
-  const wrapper = shallowMount(CountryCard).find(
-    '[class="country-card__headline"]'
-  );
-  it("renders country name properly", () => {
-    expect.assertions(1);
-    expect(wrapper.text()).toMatch(country);
+  const country = {
+    name: "Italy",
+    population: 1234556,
+    capital: "Rome",
+    flag: "https://restcountries.eu/data/ita.svg"
+  };
+  const wrapper = shallowMount(CountryCard, {
+    propsData: { country }
+  });
+
+  it("renders country name and flag properly", () => {
+    expect.assertions(2);
+    expect(wrapper.find('[class="country-card__headline"]').text()).toBe(
+      country.name
+    );
+    expect(wrapper.find("img").html()).toContain(country.flag);
   });
 });
