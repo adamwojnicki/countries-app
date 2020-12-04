a0c9932e0d7d177cd531cb0102afe5e699c15492
Adam Wojnicki <wojnicki.adam1@gmail.com>
2020-12-04 22:07:07 +0100

    Working on dynamic routes and coutry page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "@/views/Home";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import("@/views/Home.vue")
+    component: Home
   },
   {
     path: "/country/:name",
--- src/views/Country.vue
+++ src/views/Country.vue
@@ -1,10 +1,27 @@
 <template>
-  <p>{{ $route.params.name }}</p>
+  <div class="country">test</div>
 </template>
 
 <script>
 export default {
-  name: "Country"
+  name: "Country",
+  data() {
+    return {
+      country: null
+    };
+  },
+  methods: {
+    async fetchCountry() {
+      const resp = await fetch(
+        `https://restcountries.eu/rest/v2/name/${this.$route.params.name}`
+      );
+      const data = await resp.json();
+      this.country = data[0];
+    }
+  },
+  created() {
+    this.fetchCountry();
+  }
 };
 </script>
 
