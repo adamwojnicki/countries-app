fc80626afa31352bb991347f39973308d7064e46
Adam Wojnicki <wojnicki.adam1@gmail.com>
2020-12-04 21:12:50 +0100

    Added Vue router

--- src/App.bac.vue
+++ src/App.bac.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="app" :class="this.theme === 'dark' ? 'dark' : 'light'">
-    <Header />
-    <router-view />
+    <Header :theme="this.theme" />
+    <SearchBar />
+    <Loading v-if="this.loading" />
+    <Error v-if="this.error" />
+    <CountryList />
   </div>
 </template>
-<script>
-import { mapActions, mapGetters } from "vuex";
-import Header from "@/components/Header";
 
+<script>
+import { mapGetters, mapActions } from "vuex";
+import CountryList from "./components/CountryList";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import Error from "./components/Error";
+import Loading from "./components/Loading";
 export default {
   name: "App",
-  components: { Header },
+  components: { Header, CountryList, SearchBar, Error, Loading },
   methods: mapActions(["initialTheme"]),
-  computed: mapGetters(["theme"]),
+  computed: {
+    ...mapGetters(["loading", "error", "theme"])
+  },
   created() {
     this.initialTheme();
   }
 };
 </script>
+
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
 :root {
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <SearchBar />
+    <Loading v-if="this.loading" />
+    <Error v-if="this.error" />
+    <CountryList />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import CountryList from "@/components/CountryList";
+import SearchBar from "@/components/SearchBar";
+import Error from "@/components/Error";
+import Loading from "@/components/Loading";
+export default {
+  name: "Home",
+  components: { CountryList, SearchBar, Error, Loading },
+  computed: mapGetters(["loading", "error"])
+};
+</script>
+
+<style lang="scss">
+</style>
